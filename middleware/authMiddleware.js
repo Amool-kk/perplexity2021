b0583b68ccd512f13432d86a5301f83b31772550
Nirvik <nirvik.agarwal@gmail.com>
2021-03-07 13:57:29 +0530

    ADD user authentication

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+module.exports.requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check for JWT and verifying
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect("/login");
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+// Check current user
+module.exports.checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+// check if user is authenticated
+module.exports.isAuthenticated = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        next();
+      } else {
+        res.redirect("/");
+      }
+    });
+  } else {
+    next();
+  }
+};
--- models/User.js
+++ models/User.js
@@ -1,5 +1,44 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true, lowercase: true },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: [8, "min password length is 8 characters"],
+  },
 });
+
+// Fire a function before user is saved to the db
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Fire a function after doc is saved to the db
+userSchema.post("save", function (user, next) {
+  console.log("New user created and saved", user);
+  next();
+});
+
+// Static method to login users
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) return user;
+    throw Error("Incorrect Password");
+  }
+  throw Error("Incorrect Email");
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
