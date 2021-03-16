b0a71f2cfdc01f9eb750c0ec2cc1e55c9c094e13
Souparno Paul <souparnopaul@gmail.com>
2021-03-16 22:01:45 +0530

    FIX Mongo Relation

--- controllers/quesControllers.js
+++ controllers/quesControllers.js
@@ -1,8 +1,10 @@
 const Question = require("../models/Question");
 const Category = require("../models/Category");
 
-module.exports.category_get = (req, res) => {
-  res.render("category");
+module.exports.category_get = async (req, res) => {
+  // res.render("category");
+  const categories = await Category.find({});
+  res.status(200).json(categories);
 };
 
 module.exports.category_post = async (req, res) => {
@@ -17,25 +19,32 @@ module.exports.category_post = async (req, res) => {
 };
 
 module.exports.question_get = async (req, res) => {
-  const categories = await Category.find({});
-  res.render("question", { categories });
+  // const categories = await Category.find({});
+  const questions = await Question.find({});
+  res.status(200).json(questions);
+  // res.render("question", { categories });
 };
 
 module.exports.question_post = async (req, res) => {
   const { text, answer, points, duration, category_name } = req.body;
   try {
     const category = await Category.findOne({ name: category_name });
+    const cid = category._id;
+    console.log(cid);
     const question = await Question.create({
       text,
       answer,
       points,
       duration,
-      category,
+      category: cid,
     });
-    category.questions.push(question);
-    category.save();
+    await Category.updateOne(
+      { _id: cid },
+      { $push: { questions: question } }
+    );
     res.status(201).json({ question });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ errors: err });
   }
 };
