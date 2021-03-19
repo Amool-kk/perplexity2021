1d8eef72e24d4d4edda07798be5c8bbad47e4f3f
Nirvik <nirvik.agarwal@gmail.com>
2021-03-20 00:58:45 +0530

    MODIFIED index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,16 @@
 
 const socket = io.connect("http://localhost:3000");
 const currentPlayerId = document.getElementById("player-id").innerHTML;
+let bidPlayer;
 console.log(currentPlayerId);
 socket.user = currentPlayerId;
 
-socket.emit("join");
 console.log(socket.user);
+
+////////////////////////////
+// Listen for events
+////////////////////////////
+
+socket.on("start", (data) => {
+  bidPlayer = data.bidPlayer;
+});
